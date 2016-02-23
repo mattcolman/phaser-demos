f66d3616a16b78e97e846c8396bb379b019a3fd2
Matt Colman <matt.colman@blake.com.au>
2016-02-23 22:22:06 +1100

    add a base game state class with next / again buttons

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -3,10 +3,15 @@ import RainbowText from 'objects/RainbowText';
 class GameState extends Phaser.State {
 
 	create() {
-		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		text.anchor.set(0.5);
+    this.stage.backgroundColor = "#4488AA";
+    let back = this.makeButton('back', 20, 20)
+		let next = this.makeButton('next', 400, 20)
 	}
+
+  makeButton(str, x, y) {
+    var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+    this.add.text(x, y, str, style)
+  }
 
 }
 
