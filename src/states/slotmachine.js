8f2580498ffaae5affaa2d53fdae9c5867850b57
Matt Colman <matt.colman@blake.com.au>
2016-03-07 22:19:05 +1100

    spinning line

--- src/states/slotmachine.js
+++ src/states/slotmachine.js
@@ -11,20 +11,29 @@ class SlotMachine extends GameState {
 	create() {
     console.log('SlotMachine States')
 
-    let line = this.makeLine()
-
-    TweenMax.to(line, 5, {y: -1000})
+    this.line = this.makeLine()
+    this.maxHeight = this.line.height/2
+    this.speed = {y: 50}
+    TweenMax.to(this.speed, 3, {delay: 3, y: 0, ease: Back.easeOut.config(1)})
 
     super.create()
+  }
+
+  render() {
+    this.line.y += this.speed.y
+    if (this.line.y > this.maxHeight) {
+      this.line.y = 0
+    }
   }
 
   makeLine() {
     let group = this.game.add.group(this.world, 'group')
     let line  = this._makeSingleLine()
     let line2 = this._makeSingleLine()
-    line2.y = line.height
+    line2.y = -line.height
     group.addChild(line)
     group.addChild(line2)
+    group.part = [line, line2]
     return group
   }
 
@@ -33,7 +42,7 @@ class SlotMachine extends GameState {
     var group = this.game.make.group(null, 'line')
     let y = 0
     for (var i = 0; i < numSquares; i++) {
-      let avatar = this.game.add.sprite(0, y, 'avatars', "01", group)
+      let avatar = this.game.add.sprite(0, y, 'avatars', `0${i+1}`, group)
       y += avatar.height
     }
     return group
