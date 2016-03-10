6404e0559f42900edf51592dbc963772e2ba978d
Matt Colman <matt.colman@blake.com.au>
2016-03-10 22:43:02 +1100

    variable reels

--- src/states/slotmachine.js
+++ src/states/slotmachine.js
@@ -2,6 +2,8 @@ import GameState from 'states/GameState';
 import _ from 'lodash';
 import Reel from 'states/reel';
 
+const NUM_REELS = 3;
+
 class SlotMachine extends GameState {
 
   preload() {
@@ -11,40 +13,49 @@ class SlotMachine extends GameState {
 	create() {
     console.log('SlotMachine States')
 
-    this.reel1 = new Reel(this.game, 'reel1')
-    this.reel2 = new Reel(this.game, 'reel2')
-    this.reel2.x = this.reel1.width
-    this.reel3 = new Reel(this.game, 'reel3')
-    this.reel3.x = this.reel1.width*2
-    this.world.addChild(this.reel1)
-    this.world.addChild(this.reel2)
-    this.world.addChild(this.reel3)
+    var tx = 0
+    this.reels = []
+    for (var i = 0; i < NUM_REELS; i++) {
+      let r = new Reel(this.game, `reel${i}`)
+      r.x = tx
+      tx += r.width
+      this.world.addChild(r)
+      this.reels.push(r)
+    }
 
     window.SlotMachine = this;
     super.create()
   }
 
   spin() {
+    console.log('this.numReels', this.numReels)
     var results = [
       [0, 1, 1],
       [2, 2, 2],
       [3, 1, 1]
     ]
 
-    let tl = new TimelineMax()
-    tl.call(this.reel1.spin, [], this.reel1, 0)
-      .call(this.reel2.spin, [], this.reel2, .1)
-      .call(this.reel3.spin, [], this.reel3, .2)
-      .call(this.stop, [results], this, 2)
+    var tl = new TimelineMax()
+    var delay = 0
+    this.reels.forEach((reel)=>{
+      tl.call(reel.spin, [], reel, delay+=.1)
+    })
+    tl.call(this.stop, [results], this, 2)
   }
 
   stop(results) {
     let tl = new TimelineMax()
-    tl.call(this.reel1.stop, [[results[0][0], results[1][0], results[2][0]]], this.reel1, 0)
-      .call(this.reel2.stop, [[results[0][1], results[1][1], results[2][1]]], this.reel2, .1)
-      .call(this.reel3.stop, [[results[0][2], results[1][2], results[2][2]]], this.reel3, .2)
+    var i = 0
+    var delay = 0
+    this.reels.forEach((reel)=>{
+      tl.call(reel.stop, [[results[0][i], results[1][i], results[2][i]]], reel, delay+=.1)
+      i++
+    })
   }
 
 }
+
+// var p = SlotMachine.prototype;
+// p.numReels = 3
 
 export default SlotMachine;
